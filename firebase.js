e4c12ced22878966029ea3e6a35ca2a018dced87
권윤아 <01057297641a@gmail.com>
2026-04-25 04:27:16 +0900

    firebase 연결 완료

--- firebase.js
+++ firebase.js
@@ -2,13 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "여기",
-  authDomain: "여기",
-  projectId: "여기",
-  storageBucket: "여기",
-  messagingSenderId: "여기",
-  appId: "여기"
+  apiKey: "AIzaSyAIIuTA0hYxzEh_nSWE8rQ2Dj3QNBMiH40",
+  authDomain: "kuronami-4b393.firebaseapp.com",
+  projectId: "kuronami-4b393",
+  storageBucket: "kuronami-4b393.firebasestorage.app",
+  messagingSenderId: "958532247825",
+  appId: "1:958532247825:web:97b07939761bf62ddeacb0"
 };
 
 const app = initializeApp(firebaseConfig);
+
+// 🔥 이게 핵심 (DB 연결)
 export const db = getFirestore(app);
