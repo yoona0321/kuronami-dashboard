7e5756c3f908f21d722d3cf2cbb2f71af42673ac
권윤아 <01057297641a@gmail.com>
2026-04-25 01:20:13 +0900

    lol page upgrade

--- pages/lol.js
+++ pages/lol.js
@@ -1,1 +1,17 @@
 import Navbar from "../components/Navbar";
+
+export default function Lol() {
+  return (
+    <div>
+
+      {/* 🔥 메뉴바 */}
+      <Navbar />
+
+      {/* 🔥 기존 내용 */}
+      <div style={{ padding: "30px" }}>
+        <h1>테스트 페이지</h1>
+      </div>
+
+    </div>
+  );
+}
